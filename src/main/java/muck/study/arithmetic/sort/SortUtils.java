75f81bcdec92e84b36eb6089854be8e05d450de7
muck <muck@fcuh.com>
2016-05-24 14:04:05 +0800

    增加 插入排序

--- src/main/java/muck/study/arithmetic/sort/SortUtils.java
+++ src/main/java/muck/study/arithmetic/sort/SortUtils.java
@@ -59,4 +59,27 @@ public class SortUtils {
       }
     }
   }
+
+  public static int[] insertSort( int[] arr ) {
+    int i, j;
+    int insertNote;// 要插入的数据
+    int[] array = arr;
+    // 从数组的第二个元素开始循环将数组中的元素插入
+    for( i = 1; i < array.length; i++ ) {
+      // 设置数组中的第2个元素为第一次循环要播讲的数据
+      insertNote = array[i];
+      j = i - 1;
+      while(j >= 0 && insertNote < array[j]) {
+        // 如果要播讲的元素小于第j个元素,就将第j个元素向后移动
+        array[j + 1] = array[j];
+        j--;
+      }
+      System.out.println("j--->" + ( j + 1 ));
+      // 直到要插入的元素不小于第j个元素,将insertNote插入到数组中
+      array[j + 1] = insertNote;
+    }
+    return array;
+
+  }
+
 }
--- src/main/java/muck/study/arithmetic/utils/Utis.java
+++ src/main/java/muck/study/arithmetic/utils/Utis.java
@@ -1,16 +1,16 @@
 package muck.study.arithmetic.utils;
 
+
+import java.util.Arrays;
+
+
 public class Utis {
-	
-	public static long get_cur_sys_time(){
-		return System.currentTimeMillis();
-	}
-	
-	public static void printArray(int[] datas){
-		StringBuffer sb=new StringBuffer();
-		for (int i : datas) {
-			sb.append(i+",");
-		}
-		System.out.println(sb.toString());
-	}
+
+  public static long get_cur_sys_time() {
+    return System.currentTimeMillis();
+  }
+
+  public static void printArray( int[] datas ) {
+    System.out.println(Arrays.toString(datas));
+  }
 }
